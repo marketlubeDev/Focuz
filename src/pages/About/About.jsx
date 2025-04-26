07756e84e73254204e8f9f2c3aa00e7ef637c649
marketlubesDev <marketlubedevelopers@gmail.com>
2025-04-26 13:30:29 +0530

    about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h2 className="about__subtitle">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+      </h2>
+      <h1 className="about__title">Lorem ipsum dolor sit amet.</h1>
+      <div className="about__button-group">
+        <button className="btn about__button-group-book-now">Book Now</button>
+        <button className="btn about__button-group-enquiry">Enquiry</button>
+      </div>
+    </div>
+  );
 }
