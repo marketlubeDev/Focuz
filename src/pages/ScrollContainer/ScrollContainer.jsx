48f3886e5114a0b06c29bcfef9ef0d4de2c737e8
marketlubesDev <marketlubedevelopers@gmail.com>
2025-04-26 14:32:42 +0530

    scroll design

--- src/pages/ScrollContainer/ScrollContainer.jsx
+++ src/pages/ScrollContainer/ScrollContainer.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
-export default function ScrollContainer() {
-  return <div>ScrollContainer</div>;
+export default function Service() {
+  return (
+    <div className="service">
+      <div className="service__container">
+        <div className="service__container__main">
+          <h2 className="service__container__main__title">Rooms and Suits</h2>
+          <div className="service__container__main__content">
+            <div className="service__container__main__content__image">
+              <img src="" alt="service" />
+            </div>
+            <p className="service__container__main__content__description">
+              Focuz Habitat offers a range of rooms and suites, including the
+              Deluxe Suite, Two-Bedroom Suite, and Three-Bedroom Family Suite.
+              All accommodations come with modern amenities such as free Wi-Fi,
+              air conditioning, and in-room dining for a comfortable stay.
+            </p>
+          </div>
+        </div>
+
+        <div className="service__container__aside">
+          <div className="service__container__aside__image">
+            <img src="" alt="service" />
+          </div>
+          <p className="service__container__aside__title">Lorem ipsum</p>
+        </div>
+      </div>
+    </div>
+  );
 }
