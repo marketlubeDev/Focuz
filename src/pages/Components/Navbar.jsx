fdc77f63252ec3e60cfae7486477d1fd96ca4de8
marketlubesDev <marketlubedevelopers@gmail.com>
2025-04-26 15:51:54 +0530

    navbar

--- src/pages/Components/Navbar.jsx
+++ src/pages/Components/Navbar.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__content">
+          {/* Left side menu items */}
+          <div className="navbar__menu">
+            <a href="#about-us" className="navbar__menu-link">
+              About us
+            </a>
+            <a href="#services" className="navbar__menu-link">
+              Services
+            </a>
+            <a href="#stories" className="navbar__menu-link">
+              Stories
+            </a>
+          </div>
+
+          {/* Logo in center */}
+          <div className="navbar__logo">
+            <a href="#home">
+              <img src="/path-to-your-focuz-logo.png" alt="Focuz Habitat" />
+            </a>
+          </div>
+
+          {/* Book now button */}
+          <div>
+            <a href="#book-now" className="navbar__book-button">
+              Book now
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
